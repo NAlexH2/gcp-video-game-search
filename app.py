07feb628d18a636b1afff5ad91d9e6ab20647472
Alex Harris <nharris@pdx.edu>
2023-11-28 20:46:58 -0800

    do not need. may make own later

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import sys, getopt
-import flask, os
-from flask import render_template
+import sys
+from flask import Flask, request, render_template
+import os
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -10,12 +10,18 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/find_game", methods=["POST"])
+def find_game():
+    game_name = request.json["game_name"]
+    print("Itworkedorsomethingoiadmaisomdoim! ", game_name)
+    return "ok", 200
+
+
 if __name__ == "__main__":
     args = sys.argv[1:]
     if "-d" in args or "--Debug" in args:
-        app.config['TEMPLATES_AUTO_RELOAD'] = True
-        app.config['DEBUG'] = True
+        app.config["TEMPLATES_AUTO_RELOAD"] = True
+        app.config["DEBUG"] = True
         app.jinja_env.auto_reload = True
-        
-            
+
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
