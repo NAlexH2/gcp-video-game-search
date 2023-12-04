08b66c758938b74320718aa6bd1cfe98542c6b0e
Alex Harris <nharris@pdx.edu>
2023-12-03 23:14:02 -0800

    hacky job for neat trick

---
+++ index.py
@@ -0,0 +1,13 @@
+
+from flask.views import MethodView
+from flask import render_template, request
+from igdb_api import find_game
+
+class Index(MethodView):
+    def post(self):
+        if request.method == "POST":
+            return find_game(request.form["game_name"])
+        return render_template("index.html")
+
+    def get(self):
+        return render_template("index.html")
