a864acc3f91ed38b3edef695ae0d8bc25c61c1a8
Alex Harris <nharris@pdx.edu>
2023-12-04 23:07:05 -0800

    adding comments

--- index.py
+++ index.py
@@ -4,10 +4,15 @@ from flask import render_template, request
 from igdb_api import find_game
 
 class Index(MethodView):
+    
+    # A sneaky way to display a default version OR the response the user
+    # submitted to the app to produce - the on in the "if" statement.
     def post(self):
         if request.method == "POST":
             return find_game(request)
         return render_template("index.html")
 
+    # Required to have *something*, but we never use this. Flask gets very angry
+    # if we don't have both for index.
     def get(self):
         return render_template("index.html")
